9e0a542da1bfafb62c6506c4494c28f6863edb01
Lareta <lareta064@gmail.com>
2020-10-24 12:05:54 +0300

    show-offer-cards

--- src/js/main.js
+++ src/js/main.js
@@ -1,34 +1,11 @@
 $(document).ready(function () {
-    const person = {
-        name: {
-            first: 'Peter',
-            last: 'Smith',
-        },
-        age: 27,
-        role: 'admin'
-    }
-    const {
-        role = 'user'
-    } = person;
-    console.log(role);
-
-    //parallax   от Русика
-    const paralEl = document.querySelectorAll('.prlx')
-    let scrollPos = 0;
-    let direction = 0;
-    let delta = 0;
-    console.log(paralEl);
-
-    window.addEventListener('scroll', function () {
-        //get direction
-        direction = (document.body.getBoundingClientRect()).top > scrollPos ? 1 : -1;
-        delta = (document.body.getBoundingClientRect()).top - scrollPos;
-        scrollPos = (document.body.getBoundingClientRect()).top;
-
-        for (let item of paralEl) {
-            let val = item.offsetTop;
-            val += delta * 0.6;
-            item.style.top = val + "px";
+    // show more offer cards
+    let hideOfferCards = document.querySelectorAll('.hide-card');
+    const btnShowOfferCards = document.querySelector('#show-cards');
+    btnShowOfferCards.addEventListener('click', function(){
+        for(item of hideOfferCards){
+            item.classList.add('active');
+            this.classList.add('hide');
         }
-    });
+    })
 })
