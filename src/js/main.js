98376fe3408fcffb7f433dce0ad457261b994d23
Lareta <lareta064@gmail.com>
2020-10-24 15:46:12 +0300

    header

--- src/js/main.js
+++ src/js/main.js
@@ -2,10 +2,31 @@ $(document).ready(function () {
     // show more offer cards
     let hideOfferCards = document.querySelectorAll('.hide-card');
     const btnShowOfferCards = document.querySelector('#show-cards');
-    btnShowOfferCards.addEventListener('click', function(){
-        for(item of hideOfferCards){
-            item.classList.add('active');
-            this.classList.add('hide');
+    if(btnShowOfferCards){
+        btnShowOfferCards.addEventListener('click', function(){
+            for(item of hideOfferCards){
+                item.classList.add('active');
+                this.classList.add('hide');
+            }
+        });
+    }
+    //click on toggle-menu
+    const menuToggle = document.querySelector('.menu-toggle');
+    menuToggle.addEventListener('click', function () {
+        if (this.classList.contains('active')) {
+
+            this.classList.remove('active');
+            // mobMenu.classList.remove('active');
+
+            // overlayBlock.classList.remove('active');
+            // bodyEl.classList.remove('noscroll');
+
+        } else {
+            this.classList.add('active');
+            // mobMenu.classList.add('active');
+            // overlayBlock.classList.add('active');
+            // bodyEl.classList.add('noscroll');
+
         }
-    })
+    });
 })
