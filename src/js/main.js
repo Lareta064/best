0d8ee38fb420e27e3747a0de2b47046ea39e7df9
Viktoriya <v.matveyeva@yandex.ru>
2020-10-27 20:26:12 +0400

    fix

--- src/js/main.js
+++ src/js/main.js
@@ -69,3 +69,15 @@ $(document).ready(function () {
     document.querySelector('#income-val').innerHTML = 50000;
 
 })
+
+
+
+//accordion
+$('.collapse').each(function () {
+    $(this).on('show.bs.collapse', function () {
+        $(this).siblings('.card-header').children('.accordion-icon').addClass('active');
+    });
+    $(this).on('hide.bs.collapse', function () {
+        $(this).siblings('.card-header').children('.accordion-icon').removeClass('active');
+    })
+});
